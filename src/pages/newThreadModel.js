9830f8674ff1ec36c3bba02d986d7d358ae9e051
Abdul Azeez <abdaseez@gmail.com>
2019-06-29 01:04:03 +0530

    Update newThreadModel.js

--- src/pages/newThreadModel.js
+++ src/pages/newThreadModel.js
@@ -89,7 +89,7 @@ export default class CreateThreadModel extends React.Component {
 
         if (validation.isValid) {
       e.preventDefault()
-      fetch('http://localhost:5000/threads', {
+      fetch('http://34.93.54.35:8080/threads', {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
